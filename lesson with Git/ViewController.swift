27f5ddbc6b02da3227a2771fa88fed0cee0e70a9
ErbolKarmyshakov <erbolkarmysakov37@gmail.com>
2023-10-19 20:36:38 -0500

    Initial Commit

---
+++ lesson with Git/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lesson with Git
+//
+//  Created by Mac User on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("hello ")
+        print("hello ")
+    }
+
+
+}
+
